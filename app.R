8d6f3cc8e613dfc4fc39a842e192bb89f96cb863
Konstancja <galat.konstancja@gmail.com>
2023-08-21 19:40:38 +0200

    gender plot

--- app.R
+++ app.R
@@ -16,6 +16,7 @@ data <- read_excel("data/DWTdata_analysis.xlsx")
 data <- data %>%
   mutate_all(~ifelse(is.na(.), mean(., na.rm = TRUE), .))
 res <- cor(data[,-1])
+group_labels <- c("ASD", "Control", "ADHD")
 
 # UI -->
 ui <- fluidPage(
@@ -34,6 +35,10 @@ ui <- fluidPage(
              tabPanel("Gender statistics",
                       radioButtons("gender", "Choose gender:", choices = c("0", "1")),
                       verbatimTextOutput("gender_stats")
+             ),
+             
+             tabPanel("Gender",
+                      plotOutput("gender")
              ),
              
              tabPanel("Read Me",
@@ -61,6 +66,16 @@ server <- function(input, output) {
     filtered_data <- subset(data, gender == input$gender)
     summary(filtered_data)
   })
+  
+  #clinical groups vs gender
+  output$gender <- renderPlot({
+    ggplot(data, aes(x = factor(group), fill = factor(gender))) +
+      geom_bar(position = "dodge") +
+      labs(x = "Group", y = "People", fill = "Gender") +
+      scale_fill_manual(values = c("blue", "pink"), labels = c("Male", "Female")) +
+      scale_x_discrete(labels = group_labels) +
+      theme_minimal()
+})
 }
 
 # run the app -->
